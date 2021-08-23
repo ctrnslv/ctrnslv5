df983bc6f8a7045e2aeb5b164f8089a3a935ff3b
ctrnslv <87983550+ctrnslv@users.noreply.github.com>
2021-08-23 18:44:46 +0100

    x

--- config.js
+++ config.js
@@ -14,7 +14,7 @@ var config = {
             alignment: 'centered',
             hidden: false,
             //title: 'jnj',
-            description: '<audio controls> <source src="Video apertura inchiesta con song.mp3" type="audio/mpeg"></audio><br>Scorri la mappa per incontrare i luoghi e le persone dell\'inchiesta.',
+            description: '<audio controls> <source src="Audio per mappa.m4a" type="audio/mpeg"></audio><br>Scorri la mappa per incontrare i luoghi e le persone dell\'inchiesta.',
             location: {
                 center: [15.88998, 40.62668],
                 zoom:8.5,
@@ -45,7 +45,7 @@ var config = {
                       title: 'Pisticci - Movimento Tutela Val Basento',
                       image: 'https://inchiestasentinelle.webnode.it/_files/200000077-2bd632bd65/sentinella3-4.jpg',
                       video: 'https://youtu.be/JxxuBJ2epl0',
-                      description: 'Trovo Nico, insegnante di scuola e sentinella, che mi racconta delle battaglie del Movimento Tutela Val Basento per l\’ambiente lucano.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/JxxuBJ2epl0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://drive.google.com/drive/folders/1PBxVD-gW8TZVg92sXGhUyjNDGkCMh-fF">Visita le risorse online</a></p><br><p><a href="http://movimentovalbasento.altervista.org/">Visita la pagina del Movimento</a></p>',
+                      description: 'Trovo Nico, insegnante di scuola e sentinella, che mi racconta delle battaglie del Movimento Tutela Val Basento per l\’ambiente lucano.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/z6QcCOgpu1U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://drive.google.com/drive/folders/1PBxVD-gW8TZVg92sXGhUyjNDGkCMh-fF"target="_blank">Esplora gli studi sull\'impatto ambientale del petrolio </a></h3><h3><a href="http://movimentovalbasento.altervista.org/"target="_blank">Visita la pagina del Movimento</a></h3>',
 
                       location: {
                           center: [16.55850, 40.39065],
@@ -65,7 +65,7 @@ var config = {
                           hidden: false,
                           title: 'Pisticci - Studio legale',
                           image: 'https://inchiestasentinelle.webnode.it/_files/200000076-8cd838cd85/sentinella4-3.jpg',
-                          description: 'Intervisto l\'Avvocato Di Pisa, che difende le parti civili nel processo Petrolgate.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/rstAMN2-l5A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><p><a href="https://drive.google.com/drive/folders/1hBI-gmM4Up0r24OIa5GQ0jtuJUKfqHfS?usp=sharing">Accedi al materiale legale sul processo</a></p>',
+                          description: 'Intervisto l\'Avvocato Di Pisa, che difende le parti civili nel processo Petrolgate.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/rstAMN2-l5A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><h3><a href="https://drive.google.com/drive/folders/1hBI-gmM4Up0r24OIa5GQ0jtuJUKfqHfS?usp=sharing"target="_blank">Accedi al materiale legale sul processo</a></h3>',
                           location: {
                               center: [16.55850, 40.39065],
                               zoom: 14.5,
@@ -141,7 +141,7 @@ var config = {
                                           hidden: false,
                                           title: 'Viggiano - Studio medico',
                                           image: 'https://inchiestasentinelle.webnode.it/_files/200000088-432ac432af/sentinella15-5.jpg',
-                                          description:'Un dottore sentinella, il Dottor Mele, ci racconta del perchè \"in Basilicata di petrolio si muore…\"<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/W4_B4HOFr8I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://drive.google.com/drive/folders/1VaSDuX64mTLNk-JLstfpP1KbWatKdfmH?usp=sharing">Accedi agli studi sull\'impatto sanitario del petrolio</a></p>',
+                                          description:'Un dottore sentinella, il Dottor Mele, ci racconta del perchè \"in Basilicata di petrolio si muore…\"<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/W4_B4HOFr8I" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://drive.google.com/drive/folders/1VaSDuX64mTLNk-JLstfpP1KbWatKdfmH?usp=sharing"target="_blank">Accedi agli studi sull\'impatto sanitario del petrolio</a></h3>',
                                           location: {
                                               center: [15.90501,40.33897],
                                               zoom: 14,
@@ -179,7 +179,7 @@ var config = {
                                                   hidden: false,
                                                   title: 'Viggiano - Centro Oli',
                                                   //image: ,
-                                                  description: 'Percorriamo il perimetro del Centro Oli, dove mura colorate stridono con la realtà estrattiva racchiusa all\'interno.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/HsSyVOTEiQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://www.eni.com/eni-basilicata/chi-siamo/centro-olio-val-d-agri.page">Visita la pagina del Centro Oli</a></p>',
+                                                  description: 'Percorriamo il perimetro del Centro Oli, dove mura colorate stridono con la realtà estrattiva racchiusa all\'interno.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/HsSyVOTEiQM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://www.eni.com/eni-basilicata/chi-siamo/centro-olio-val-d-agri.page"target="_blank">Visita la pagina del Centro Oli</a></h3>',
                                                   location: {
                                                       center: [15.89166, 40.31453],
                                                       zoom: 13,
@@ -256,7 +256,7 @@ var config = {
                                                                   hidden: false,
                                                                   title: 'Pisticci Scalo - Tecnoparco',
                                                                   //image: ,
-                                                                  description: 'Visito con Mimmo il Tecnoparco di Pisticci Scalo, fonte di tormento per gli abitanti della zona.<p><a href="https://drive.google.com/drive/folders/1kSpIWE8o_vRa-6D-DOVTN6a5ZdOgfDKQ?usp=sharing ">Vai a risorse e grafici </a></p>',
+                                                                  description: 'Visito con Mimmo il Tecnoparco di Pisticci Scalo, fonte di tormento per gli abitanti della zona.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/XChlgeStumk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><h3><a href="https://drive.google.com/drive/folders/1kSpIWE8o_vRa-6D-DOVTN6a5ZdOgfDKQ?usp=sharing "target="_blank">Vai a risorse e grafici </a></h3>',
                                                                   location: {
                                                                       center: [16.548939,40.420474],
                                                                       zoom: 13,
@@ -294,7 +294,7 @@ var config = {
                                                                           hidden: false,
                                                                           title: 'Corleto Perticara - Tempa Rossa',
                                                                           //image: ,
-                                                                          description: 'Ci avviciniamo al sito estrattivo di Tempa Rossa.<r><br><br> <iframe width="350" height="250" src="https://www.youtube.com/embed/6F1mWhUrbMg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://www.it.total.com/it/pagine/attivita/il-progetto-tempa-rossa ">Visita la pagina di Tempa Rossa</a></p> ',
+                                                                          description: 'Ci avviciniamo al sito estrattivo di Tempa Rossa.<r><br><br> <iframe width="350" height="250" src="https://www.youtube.com/embed/6F1mWhUrbMg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://www.it.total.com/it/pagine/attivita/il-progetto-tempa-rossa "target="_blank">Visita la pagina di Tempa Rossa</a></h3> ',
                                                                           location: {
                                                                               center: [16.085372,40.409225],
                                                                               zoom: 13.5,
@@ -313,7 +313,7 @@ var config = {
                                                                               hidden: false,
                                                                               title: 'Corleto Perticara - Pascolo',
                                                                               //image:,
-                                                                              description: 'Osservo Mimmo mentre rimuove uno strumento di monitoraggio del gas flaring attraverso il quale ha realizzato un time lapse per Cova Contro.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/jFcEa9Z-EHI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://covacontro.org/# ">Visita la pagina di Cova Contro</a></p>',
+                                                                              description: 'Osservo Mimmo mentre rimuove uno strumento di monitoraggio del gas flaring attraverso il quale ha realizzato un time lapse per Cova Contro.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/jFcEa9Z-EHI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://covacontro.org/# "target="_blank">Visita la pagina di Cova Contro</a></h3>',
                                                                               location: {
                                                                                   center: [16.085372,40.409225],
                                                                                   zoom: 13.5,
@@ -330,7 +330,7 @@ var config = {
                                                                                       id: 'qq-style-id',
                                                                                       alignment: 'left',
                                                                                       hidden: false,
-                                                                                      title: 'Pietrapertosa - Dolomiti lucane',
+                                                                                      title: 'Pietrapertosa - Dolomiti Lucane',
                                                                                       //image:' <img src="Dolomiti lucane.jpg">' ,
                                                                                       description: ' <img src="Dolomiti lucane.jpg"><br><br>Ammiro le affascinanti dolomiti lucane che quasi mi fanno dimenticare dei tanti pozzi a pochi chilometri di distanza.<br><br> <iframe width="350" height="250" src="https://www.youtube.com/embed/8Z1beGcgvL4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
                                                                                       location: {
@@ -389,7 +389,7 @@ var config = {
                                                                                                   hidden: false,
                                                                                                   title: 'Potenza - Università',
                                                                                                   image: 'https://inchiestasentinelle.webnode.it/_files/200000081-946ee946f0/sentinella7-6.jpg',
-                                                                                                  description: 'Incontro Luca Manes che mi racconta del lavoro della ONG Recommon a tutela delle comunità che fronteggiano multinazionali.<br><br><p><a href="https://www.recommon.org/en/">Vai alla pagina di Recommon </a></p><br><p><a href="https://inganno.recommon.org/ ">Vai all\'inchiesta di Recommon sulla Basilicata </a></p>',
+                                                                                                  description: 'Incontro Luca Manes che mi racconta del lavoro della ONG Recommon a tutela delle comunità che fronteggiano multinazionali.<br><br><h3><a href="https://www.recommon.org/en/">Vai alla pagina di Recommon </a></h3><h3><a href="https://inganno.recommon.org/ "target="_blank">Vai all\'inchiesta di Recommon sulla Basilicata </a></h3>',
                                                                                                   location: {
                                                                                                       center: [15.805873,40.633602],
                                                                                                       zoom: 13,
@@ -408,7 +408,7 @@ var config = {
                                                                                                       hidden: false,
                                                                                                       title: 'Potenza - ARPAB',
                                                                                                     //  image: ,
-                                                                                                      description: 'Intervisto l\'Avvocato Tisci, Direttore dell\'Agenzia Regionale per la Protezione dell\'Ambiente Basilicata.<br><br> <iframe width="350" height="250" src="https://www.youtube.com/embed/DiViQZJFe9A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="http://www.arpab.it/">Visita la pagina dell\'ARPAB</a></p> ',
+                                                                                                      description: 'Intervisto l\'Avvocato Tisci, Direttore dell\'Agenzia Regionale per la Protezione dell\'Ambiente Basilicata.<br><br> <iframe width="350" height="250" src="https://www.youtube.com/embed/DiViQZJFe9A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="http://www.arpab.it/"target="_blank">Visita la pagina dell\'ARPAB</a></h3> ',
                                                                                                       location: {
                                                                                                           center: [15.805873,40.633602],
                                                                                                           zoom: 13,
@@ -427,7 +427,7 @@ var config = {
                                                                                                 hidden: false,
                                                                                                 title: 'Potenza - Redazione Basilicata24',
                                                                                               //  image: ,
-                                                                                                description: 'Incontro i direttori della testata giornalistica d\’inchiesa Basilicata24 che mi raccontano dei rischi che corrono sia i giornalisti che denunciano che le loro fonti, i cittadini.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/PBX0bcNh7sc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><p><a href="https://www.basilicata24.it/ ">Visita la pagina di Basilicata24 </a></p>',
+                                                                                                description: 'Incontro i direttori della testata giornalistica d\’inchiesa Basilicata24 che mi raccontano dei rischi che corrono sia i giornalisti che denunciano che le loro fonti, i cittadini.<br><br><iframe width="350" height="250" src="https://www.youtube.com/embed/PBX0bcNh7sc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe><br><br><h3><a href="https://www.basilicata24.it/ "target="_blank">Visita la pagina di Basilicata24 </a></h3>',
                                                                                                 location: {
                                                                                                     center: [15.77840, 40.63997],
                                                                                                     zoom: 13,
